b2d8b3bd273ebaf9b1016c5a6310792771d60e7f
vbarbarossa <vbarbarossa@science.ru.nl>
2019-09-20 11:45:31 +0200

    updated script: cleaning, filtering, removing duplicates, checking for fishbase synonyms

--- compile_occurrence_records.R
+++ compile_occurrence_records.R
@@ -1,55 +1,118 @@
 #Valerio Barbarossa
 # stript that compiles the occurrence records from the different datasets
-# ala.org.ay
+
 library(valerioUtils)
 libinv(c('dplyr','vroom'))
 
+diag <- function(df,name_df = '',file_out = ''){
+  cat('Dataset: ',name_df,'\n',
+  'No. records: ',prettyNum(nrow(df),big.mark = ','),'\n',
+  'No. species: ',prettyNum(length(unique(df$name)),big.mark = ','),'\n\n\n',
+  file = file_out)
+}
+
+# read reference names that should be used to extract species from the datasets----------------------------
+tab <- read.csv('proc/names_fishbase.csv',stringsAsFactors = F)
+
+# separate unique names from names with synonyms for later use
+tab_names <- tab %>%
+  filter(name == name_synonym)
+tab_syn <- tab %>%
+  filter(name != name_synonym)
+
+#assign names that will be used for filtering
+filter_names <- unique(tab$name_synonym)
+
+# clean and filter datasets--------------------------------------------------------------------------------
+# setup location of datasets
 dir_data <- '../data/fish_databases/'
 
-#ala
+# ala.org.ay
 ala <- vroom(paste0(dir_data,'ala.org.au/Fishes-brief.csv'),delim = ',') %>%
   select(name = scientificName,lon = decimalLongitude,lat = decimalLatitude) %>%
-  .[!is.na(.$lon) & !is.na(.$name),]
-#~3M occ
-#7,358 names
+  .[!is.na(.$lon) & !is.na(.$name),] %>%
+  filter(name %in% filter_names)
+diag(ala,'ala.org.ay','filtering_occurrence_datasets_diag.log')
 
-#boldsystems
+#boldsystems #not implemented
 
 #fishnet2
 fishnet <- vroom(paste0(dir_data,'fishnet2/fishnet2.csv')) %>%
   select(name = ScientificName,lon = Longitude,lat = Latitude) %>%
-  .[!is.na(.$lon) & !is.na(.$name),]
+  .[!is.na(.$lon) & !is.na(.$name),] %>%
+  filter(name %in% filter_names)
+diag(fishnet,'fishnet2','filtering_occurrence_datasets_diag.log')
 #~4M occ
 #62,747 names
 
 #gbif
 gbif <- vroom(paste0(dir_data,'gbif/actinopterygii.csv')) %>%
   select(name = species,lon = decimalLongitude,lat = decimalLatitude) %>%
-  .[!is.na(.$lon) & !is.na(.$name),]
+  .[!is.na(.$lon) & !is.na(.$name),] %>%
+  filter(name %in% filter_names)
+diag(gbif,'gbif','filtering_occurrence_datasets_diag.log')
 #~17M occ
 #62,747 names
 
 #portalbiodiversidade.icmbio.gov.br
 bra <- vroom(paste0(dir_data,'portalbiodiversidade.icmbio.gov.br/portalbio_export_17-09-2019-10-21-11.csv')) %>%
   select(name = Especie,lon = Longitude,lat = Latitude) %>%
-  .[.$lon != "Acesso Restrito" & .$name != "Sem Informações",]
+  .[.$lon != "Acesso Restrito" & .$name != "Sem Informações",] %>%
+  filter(name %in% filter_names)
+diag(bra,'portalbiodiversidade.icmbio.gov.br','filtering_occurrence_datasets_diag.log')
 #~80k occ
 #2,701 names
 
 #splink.org
 splink <- vroom(paste0(dir_data,'splink.org/speciesLink_all_112728_20190917101630.txt')) %>%
   select(name = scientificname,lon = longitude,lat = latitude) %>%
-  .[!is.na(.$lon) & !is.na(.$name),]
+  .[!is.na(.$lon) & !is.na(.$name),] %>%
+  filter(name %in% filter_names)
+diag(splink,'splink.org','filtering_occurrence_datasets_diag.log')
 
-# read reference names that should be used to filter the data
-tab <- read.csv('proc/names_fishbase.csv',stringsAsFactors = F)
+# combine records from different datasets------------------------------------------------------------------
 
 # bind and filter occurrence records
 occ <- rbind(ala,fishnet,gbif,bra,splink) %>%
-  filter(name %in% tab$name_synonym) %>%
-  select(name_src = name,lon,lat)
+  arrange(name) %>%
+  distinct()
 
-o2 <- left_join(occ,tab,by=c('name_src'='name_synonym'))
-# 2.2M records
-#
+# there are still some lat and lon coordinates with letters in there e.g., 1044730E
+# difficult to convert without knowing the CRS, filter out!
+# create vector to report rows with letters in latitude or longitude
+
+# understand unique symbols to take out
+unique_symbols <- strsplit(paste0(occ$lon,occ$lat),'') %>%
+  do.call('c',.) %>%
+  unique(.)
+# exclude everything that is not numeric (numbers,.,-)
+to_exclude <- unique_symbols[!unique_symbols %in% c(as.character(0:9),'.','-')] 
+lonlat_to_exclude <- lapply(strsplit(paste0(occ$lon,occ$lat),''),
+                          function(x) sum(x %in% to_exclude)> 0 ) %>%
+  do.call('c',.)
+
+# and filter occ
+cat('Removing ',prettyNum(sum(lonlat_to_exclude),big.mark = ','),' records with letters in coordinates\n')
+occ <- occ %>%
+  filter(!lonlat_to_exclude) %>%
+  mutate(name,lon = as.numeric(lon),lat = as.numeric(lat)) # transform lat lon to numeric
+
+
+# merge with fishbase synonyms----------------------------------------------------------------------------
+# adjust columns of reference table for synonyms
+tab_syn <- tab_syn %>% select(name_fishbase = name,name = name_synonym)
+
+occ_syn <- right_join(occ,tab_syn,by='name') %>% #select only records that are synonyms
+  select(name = name_fishbase,lon,lat) # and assign the fishbase name to the name
+occ_nosyn <- occ %>%
+  filter(!name %in% tab_syn$name)
+
+diag(occ_nosyn,'merged occurrence records (no synonyms)','filtering_occurrence_datasets_diag.log')
+diag(occ_nosyn,'additional merged occurrence records from fishbase synonyms','filtering_occurrence_datasets_diag.log')
+
+occ_total <- rbind(occ_nosyn,occ_syn) %>%
+  arrange(name) %>%
+  distinct()
+diag(occ_nosyn,'final occurrence records cleaned and checked for synonyms','filtering_occurrence_datasets_diag.log')
+
 
