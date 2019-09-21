159d194451dbc399426bbfd26a7c807a8cc45f7a
vbarbarossa <vbarbarossa@science.ru.nl>
2019-09-21 17:15:31 +0200

    fi

--- R/compile_occurrence_records.R
+++ R/compile_occurrence_records.R
@@ -4,18 +4,56 @@
 library(valerioUtils)
 libinv(c('dplyr','vroom'))
 
-diag <- function(df,name_df = '',file_out = ''){
+# diagnostics file
+file_diag <- 'filtering_occurrence_datasets_diag.log'
+
+diag <- function(df,name_df = '',file_out = '',append = T){
+  
   cat('Dataset: ',name_df,'\n',
   'No. records: ',prettyNum(nrow(df),big.mark = ','),'\n',
   'No. species: ',prettyNum(length(unique(df$name)),big.mark = ','),'\n\n\n',
-  file = file_out,append = T)
+  file = file_out,append = append)
 }
 
 # read reference names that should be used to extract species from the datasets----------------------------
-tab <- read.csv('proc/names_fishbase.csv',stringsAsFactors = F)
+# tab <- read.csv('proc/names_fishbase.csv',stringsAsFactors = F)
+tab <- vroom(list.files('proc/',pattern = 'iucn_names',full.names = T))
 
 #assign names that will be used for filtering
-filter_names <- unique(tab$name_synonym)
+# filter_names <- unique(tab$name_synonym)
+filter_names <- unique(c(tab$name_iucn,tab$name_iucn_synonym,tab$name_src)) %>%
+  .[!is.na(.)]
+
+# functions to clean nomenclature--------------------------------------------------------------------------
+
+# function that removes any trailing word with capital (e.g., Linneaus)
+rm_author <- function(x){
+  s <- strsplit(x,' ')
+  if(length(s[[1]]) > 2){
+    s3 <- strsplit(s[[1]][3],'')
+    if(s3[[1]][1] %in% LETTERS){
+      return(
+        paste(unlist(s)[1:2],collapse=' ')
+        )
+    }else{
+      return(x)
+    }
+  }else{
+    return(x)
+  }
+}
+
+# function that removes anything after , or ( and trailing capitals
+clean_binomial <- function(names){
+    cl <- lapply(strsplit(names,','),function(x) x[[1]][1]) %>% 
+    unlist() %>%
+    strsplit(.,'\\(') %>%
+    lapply(.,function(x) x[[1]][1]) %>%
+    trimws() %>%
+    lapply(.,rm_author) %>%
+    unlist
+    return(cl)
+}
 
 # clean and filter datasets--------------------------------------------------------------------------------
 # setup location of datasets
@@ -25,8 +63,9 @@ dir_data <- '../data/fish_databases/'
 ala <- vroom(paste0(dir_data,'ala.org.au/Fishes-brief.csv'),delim = ',') %>%
   select(name = scientificName,lon = decimalLongitude,lat = decimalLatitude) %>%
   .[!is.na(.$lon) & !is.na(.$name),] %>%
+  mutate(name = clean_binomial(name)) %>%
   filter(name %in% filter_names)
-diag(ala,'ala.org.au','filtering_occurrence_datasets_diag.log')
+diag(ala,'ala.org.au',file_diag,append = F)
 
 #boldsystems #not implemented
 
@@ -34,32 +73,37 @@ diag(ala,'ala.org.au','filtering_occurrence_datasets_diag.log')
 fishnet <- vroom(paste0(dir_data,'fishnet2/fishnet2.csv')) %>%
   select(name = ScientificName,lon = Longitude,lat = Latitude) %>%
   .[!is.na(.$lon) & !is.na(.$name),] %>%
+  mutate(name = clean_binomial(name)) %>%
   filter(name %in% filter_names)
-diag(fishnet,'fishnet2','filtering_occurrence_datasets_diag.log')
+diag(fishnet,'fishnet2',file_diag)
 
 
-#gbif
+#gbif <<<<<<<<< problem with names, need to extract from string to match
 gbif <- vroom(paste0(dir_data,'gbif/actinopterygii.csv')) %>%
   select(name = species,lon = decimalLongitude,lat = decimalLatitude) %>%
   .[!is.na(.$lon) & !is.na(.$name),] %>%
+  mutate(name = clean_binomial(name)) %>%
   filter(name %in% filter_names)
-diag(gbif,'gbif','filtering_occurrence_datasets_diag.log')
+diag(gbif,'gbif',file_diag)
+# need to add step to extract scientificName
 
 
 #portalbiodiversidade.icmbio.gov.br
 bra <- vroom(paste0(dir_data,'portalbiodiversidade.icmbio.gov.br/portalbio_export_17-09-2019-10-21-11.csv')) %>%
   select(name = Especie,lon = Longitude,lat = Latitude) %>%
   .[.$lon != "Acesso Restrito" & .$name != "Sem Informações",] %>%
+  mutate(name = clean_binomial(name)) %>%
   filter(name %in% filter_names)
-diag(bra,'portalbiodiversidade.icmbio.gov.br','filtering_occurrence_datasets_diag.log')
+diag(bra,'portalbiodiversidade.icmbio.gov.br',file_diag)
 
 
 #splink.org
 splink <- vroom(paste0(dir_data,'splink.org/speciesLink_all_112728_20190917101630.txt')) %>%
   select(name = scientificname,lon = longitude,lat = latitude) %>%
   .[!is.na(.$lon) & !is.na(.$name),] %>%
+  mutate(name = clean_binomial(name)) %>%
   filter(name %in% filter_names)
-diag(splink,'splink.org','filtering_occurrence_datasets_diag.log')
+diag(splink,'splink.org',file_diag)
 
 # combine records from different datasets------------------------------------------------------------------
 
@@ -84,7 +128,7 @@ lonlat_to_exclude <- lapply(strsplit(paste0(occ$lon,occ$lat),''),
 
 # and filter occ
 cat('Removing ',prettyNum(sum(lonlat_to_exclude),big.mark = ','),' records with letters in coordinates\n',
-    file = 'filtering_occurrence_datasets_diag.log',append = T)
+    file = file_diag,append = T)
 
 occ <- occ %>%
   filter(!lonlat_to_exclude) %>%
@@ -103,14 +147,14 @@ occ_syn <- right_join(occ,tab_syn,by='name') %>% #select only records that are s
 occ_nosyn <- occ %>%
   filter(!name %in% tab_syn$name)
 
-diag(occ_nosyn,'merged occurrence records (no synonyms)','filtering_occurrence_datasets_diag.log')
-diag(occ_syn,'additional merged occurrence records from fishbase synonyms','filtering_occurrence_datasets_diag.log')
+diag(occ_nosyn,'merged occurrence records (no synonyms)',file_diag)
+diag(occ_syn,'additional merged occurrence records from fishbase synonyms',file_diag)
 
 occ_total <- rbind(occ_nosyn,occ_syn) %>%
   arrange(name) %>%
   distinct()
 
-diag(occ_total,'final occurrence records cleaned and checked for synonyms','filtering_occurrence_datasets_diag.log')
+diag(occ_total,'final occurrence records cleaned and checked for synonyms',file_diag)
 
 vroom_write(occ_total,'proc/compiled_occurrence_records.tsv.gz')
 saveRDS(occ_total,'proc/compiled_occurrence_records.rds')
--- R/ranges_dianostics.R
+++ R/ranges_dianostics.R
@@ -3,3 +3,105 @@
 # and runs the following diagnostics:
 # 1) calculates total range area per species
 # 2) plots a map of the obtained ranges, number of species per HB unit
+
+library(valerioUtils)
+libinv(c('dplyr','vroom','foreign','sf'))
+
+HB_lev <- '08'
+
+# files from ranges2HB script
+range2HB_files <- list.files(path = 'proc/occurrence_records_on_hb',HB_lev,'/', full.names = T)
+
+# set data directory where hydrobasins folder is store
+dir_data <- '../data/'
+dir_figs <- dir_('figs/')
+
+# compile table ranges
+tab <- lapply(range2HB_files,readRDS) %>%
+  do.call('rbind',.) %>%
+  as_tibble() %>%
+  distinct() %>%
+  select(name,HYBAS_ID = hybas_id)
+
+cat('Number of species successfully referenced on HB: ',prettyNum(length(unique(tab$name)),big.mark = ','),'\n',
+    'Number of HB units: ',prettyNum(nrow(tab),big.mark = ','),
+    file = 'ranges_diagnostics.log')
+
+# load HB
+HB <- lapply(c('af','ar','as','au','eu','gr','na','sa','si'),
+             function(x) read.dbf(paste0(dir_data,'HydroBASINS/global_lev',HB_lev,'/hybas_',x,'_lev',HB_lev,'_v1c.dbf'))) %>%
+  do.call('rbind',.) %>%
+  as_tibble()
+
+tab_HB <- left_join(tab,HB)
+
+# 1) calculate area per species
+area <- tab_HB %>%
+  group_by(name) %>%
+  summarize(range_area = sum(SUB_AREA))
+
+# 2) no species per HB
+count <- tab_HB %>%
+  group_by(HYBAS_ID) %>%
+  summarize(no_species = n())
+
+# load sf features for HB
+HB_sf <- lapply(c('af','ar','as','au','eu','gr','na','sa','si'),
+             function(x) st_read(paste0(dir_data,'HydroBASINS/global_lev',HB_lev,'/hybas_',x,'_lev',HB_lev,'_v1c.shp'))) %>%
+  do.call('rbind',.)
+
+count_sf <- right_join(HB_sf,count) # need to put first sf data to keep the sf type
+
+pdf(paste0(dir_figs,'ranges_count',HB_lev,'.pdf'))
+plot(count_sf['no_species'],border = NA)
+dev.off()
+
+# # 3) plot also occurrence points
+# occ <- readRDS('proc/compiled_occurrence_records.rds')
+# # convert to spatial points
+# pts <- occ %>% 
+#   filter(!is.na(lon) & !is.na(lat)) %>% #make sure there are no NAs in the coords
+#   filter(lon >= -180 & lon <= 180) %>%
+#   filter(lat >= -90 & lat <= 90) %>%
+#   st_as_sf(coords = c('lon','lat'),crs=4326) #convert to sf
+# 
+# # and plot
+# pdf(paste0(dir_figs,'occurrence_records.pdf'))
+# plot(st_geometry(pts),cex = 0.01,pch=20)
+# dev.off()
+# 
+# # basins with occ
+# pdf(paste0(dir_figs,'occurrence_records_on_ranges.pdf'))
+# plot(st_geometry(count_sf),col = 'gray90',border = NA)
+# plot(st_geometry(pts),cex = 0.01,pch=20,add = TRUE)
+# dev.off()
+
+# 4) compare area with species in common with IUCN
+# iucn <- lapply(1:2,
+#                function(x) foreign::read.dbf(paste0(dir_data,'IUCN/FW_FISH_20181113/FW_FISH_PART_',x,'.dbf'))) %>%
+#   do.call('rbind',.) %>%
+#   as_tibble()
+# 
+# iucn_area <- iucn %>%
+#   group_by(binomial) %>%
+#   summarize(range_area_iucn = sum(shape_Area)) %>%
+#   select(name = binomial,range_area_iucn)
+# 
+# valerioUtils::r.squared(compare_area$range_area,compare_area$range_area_iucn) 
+# hmmm iucn area looks weird should compare both of them based on HB12 table
+# then retrieve data from connectfish
+
+iucn_HB <- readRDS(paste0(dir_data,'hybas12_fish_w_area.rds')) %>%
+  as_tibble() %>%
+  select(name = binomial, HYBAS_ID, SUB_AREA) %>%
+  arrange(name) %>%
+  distinct()
+area_iucn_HB <- iucn_HB %>%
+  group_by(name) %>%
+  summarize(range_area_iucn = sum(SUB_AREA))
+
+# compare area tab
+compare_area <- inner_join(area,area_iucn_HB)
+valerioUtils::r.squared(compare_area$range_area,compare_area$range_area_iucn) 
+
+
